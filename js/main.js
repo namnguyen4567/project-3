bea256c08105d0b6612253c572a4bb7b4ff2d572
Nam Nguyen <103019275+namnguyen4567@users.noreply.github.com>
2022-12-07 12:55:51 -0600

    add burger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-// JS scripts placed here
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
